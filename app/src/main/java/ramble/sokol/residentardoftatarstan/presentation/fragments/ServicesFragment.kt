634c4f6f94414c7dd044257017d8a94885a90e67
RambleTOT <rambletot@gmail.com>
2024-08-20 01:32:27 +0500

    map

--- app/src/main/java/ramble/sokol/residentardoftatarstan/presentation/fragments/ServicesFragment.kt
+++ app/src/main/java/ramble/sokol/residentardoftatarstan/presentation/fragments/ServicesFragment.kt
@@ -6,15 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ramble.sokol.residentardoftatarstan.R
+import ramble.sokol.residentardoftatarstan.databinding.FragmentMapBinding
+import ramble.sokol.residentardoftatarstan.databinding.FragmentServicesBinding
 
 class ServicesFragment : Fragment() {
+
+    private var binding: FragmentServicesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false)
+        binding = FragmentServicesBinding.inflate(inflater, container, false)
+        val view = binding!!.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init(){
+
+        binding!!.cardMap.setOnClickListener {
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.layout_fragment, MapFragment())
+            transaction.disallowAddToBackStack()
+            transaction.commit()
+        }
+
     }
 
 }
