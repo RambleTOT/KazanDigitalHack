e0ddb4119693256eb4c53564894c8465dc123841
RambleTOT <rambletot@gmail.com>
2024-08-23 23:46:06 +0500

    create nfc

--- app/src/main/java/ramble/sokol/residentardoftatarstan/presentation/activity/MainActivity.kt
+++ app/src/main/java/ramble/sokol/residentardoftatarstan/presentation/activity/MainActivity.kt
@@ -1,13 +1,18 @@
 package ramble.sokol.residentardoftatarstan.presentation.activity
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.yandex.mapkit.MapKitFactory
 import ramble.sokol.residentardoftatarstan.BuildConfig
 import ramble.sokol.residentardoftatarstan.R
+import ramble.sokol.residentardoftatarstan.presentation.fragments.MainFragment
 import ramble.sokol.residentardoftatarstan.presentation.fragments.SplashScreenFragment
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val MAPKIT_API_KEY = BuildConfig.MAP_KEY
     }
+
+    private lateinit var nfcAdapter: NfcAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +37,45 @@ class MainActivity : AppCompatActivity() {
 
             supportFragmentManager.findFragmentByTag(SplashScreenFragment::class.java.simpleName)
 
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        // Проверка на наличие NFC
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC не поддерживается", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
         if (fragment !is SplashScreenFragment){
             supportFragmentManager.beginTransaction()
                 .add(R.id.layout_fragment, splashScreenFragment, SplashScreenFragment::class.java.simpleName)
                 .commit()
         }
 
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        intent?.let {
+            val fragment = supportFragmentManager.findFragmentById(R.id.layout_fragment) as? MainFragment
+            fragment?.handleNfcIntent(it)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Настройка фильтра для NFC
+        val intent = Intent(this, javaClass).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val filters = arrayOf(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter.disableForegroundDispatch(this)
     }
 
     override fun onStart() {
--- app/src/main/java/ramble/sokol/residentardoftatarstan/presentation/fragments/MainFragment.kt
+++ app/src/main/java/ramble/sokol/residentardoftatarstan/presentation/fragments/MainFragment.kt
@@ -1,6 +1,10 @@
 package ramble.sokol.residentardoftatarstan.presentation.fragments
 
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,6 +37,19 @@ class MainFragment : Fragment() {
             transaction.replace(R.id.layout_fragment, OnBoardingFragment())
             transaction.disallowAddToBackStack()
             transaction.commit()
+        }
+    }
+
+    fun handleNfcIntent(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action ||
+            NfcAdapter.ACTION_TECH_DISCOVERED == action ||
+            NfcAdapter.ACTION_TAG_DISCOVERED == action) {
+
+            // Обработка данных из NFC метки
+            val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+            // Ваш код для работы с меткой
+            Log.d("MyLog", tag.toString())
         }
     }
 
